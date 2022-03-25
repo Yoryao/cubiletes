fb2de3e852f811275a85a681179f4e816a41b28d
yoryao <jorgejoaquinrivera@gmail.com>
2022-03-25 15:44:30 -0300

    solved problem with images, continue with logic to turn cards

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Card from "./components/Card";
 function App() {
   return (
     <>
-    
-        <Card img="true"></Card>
-        <Card img="true"></Card>
-        <Card img="false"></Card>
-    
+            <div>PROBANDO EL DIV PARA LAS CARTAS</div>
+
+        <Card img={true}></Card>
+        <Card img={false}></Card>
+        <Card img={true}></Card>
     
     
     
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import red from     "../../src/red_heart_card.png"
+import black from   "../../src/spade_black_card.png"
+import reverse from   "../../src/reverse_card.png"
+
+
+
 
 const Card = (props) => {
   return (
     <>
-        <div>PROBANDO EL DIV PARA LAS CARTAS
-        
-        {props.img == "true" ? <img src="../../public/roja.png" alt='Red_card'/> : <img src="../../public/roja.png" alt="Black_card"/>};
-
-
-
-        </div>
     
+        
+        {props.img === true ? <img src={red} alt="RedCard" width={100} height={150} /> : <img src={black} alt="BlackCard" width={100} height={150} />}
+        {props.img === true ? <img src={reverse} alt="ReverseCard" width={100} height={150} /> : <img src={black} alt="BlackCard" width={100} height={150} />}
+        
+        
+
+
+        
     </>
   )
 }
